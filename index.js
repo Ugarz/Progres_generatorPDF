3d404d768a12ddff2500a62c852c9d5607ce91ad
ugo <ugo.arzur@morning.com>
2017-03-16 14:31:58 +0100

    push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var PDFDocument = require('pdfkit');
 var moment = require('moment');
+var devis = require('./devis.json');
+var facture = require('./facture.json');
 
 var localLocale = moment();
 
@@ -8,9 +10,8 @@ localLocale.locale('fr'); // set this instance to use French
 localLocale.format('LLLL'); // dimanche 15 juillet 2012 11:01
 
 
-
 /*----------  Variables  ----------*/
-var CurrentDate = localLocale.format('LL');
+var CurrentFullDate = localLocale.format('LL');
 
 var sec = moment().second();
 var min = moment().minute();
@@ -18,25 +19,41 @@ var hour = moment().hour();
 var fullHour = hour + ':' + min + ':' + sec;
 var day = moment().day(); // of the week
 var date = moment().date(); // of the month
-
+moment().format();
 
 
 /*----------  Create pdf  ----------*/
-doc = new PDFDocument;
-doc.pipe(fs.createWriteStream('pdf_sortie.pdf'));
-console.log('pdf_sortie.pdf printed')
 
-moment().format();
-doc.fontSize(10);
-doc.text('Nous sommes actuellement le '+ CurrentDate + '. Il est ' + fullHour);
-doc.text('Nous sommes le '+ day + 'e jour de la semaine !');
-doc.fontSize(15);
-doc.text('\n');
-doc.text('Petit saut de ligne');
-doc.text('\n');
-doc.text('Votre programme pour le '+ date +' est arrivé !');
-doc.text('\n');
-doc.fontSize(12);
-doc.text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quidem, voluptatibus! Maxime veniam dolor atque repellendus, quibusdam provident eum aliquid quaerat, necessitatibus sed, quisquam saepe beatae unde neque minima cupiditate?');
-doc.text (('Ce message est fixé en absolute en X et Y !'), 300, 300)
-doc.end();
+function paragraphe({ data : text, }){
+
+}
+function createpdf(document){
+    console.log('\n **** Création du pdf:')
+    doc = new PDFDocument;
+    doc.pipe(fs.createWriteStream('pdf_sortie.pdf'));
+
+
+    doc.fontSize(20)
+        .text(`${document.informations.type}`)
+        .text('\n');
+
+    doc.fontSize(10)
+        .text(`Expéditeur : ${document.user1}`)
+        .text('\n');
+
+    doc.fontSize(14)
+        .text('Ce devis a été réalisé le '+ CurrentFullDate)
+        .text('Votre programme pour le '+ date +' est arrivé !')
+        .text('\n');
+
+    doc.fontSize(10)
+        .text('Nous sommes actuellement le '+ CurrentFullDate + '. Il est ' + fullHour)
+        .text('Nous sommes le '+ day + 'e jour de la semaine !')
+        .text('\n');
+
+    doc.fontSize(12)
+        .text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quidem, voluptatibus! Maxime veniam dolor atque repellendus, quibusdam provident eum aliquid quaerat, necessitatibus sed, quisquam saepe beatae unde neque minima cupiditate?')
+        .end();
+    console.log('pdf_sortie.pdf printed')
+}
+createpdf(devis)
